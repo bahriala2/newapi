3d10d566c33f686f0f6d2ed3250ab3501cce52ae
bahriala2 <94655960+bahriala2@users.noreply.github.com>
2024-12-06 06:09:29 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,7 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-import numpy as np
-from keras.models import load_model
+from fastapi import FastAPI
 
-# Initialiser l'application FastAPI
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "API is working!"}
+    return {"message": "Hello World"}
